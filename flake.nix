3250a332e008a6b4f3fc096409763664d1845cf3
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2025-06-06 03:48:19 +0000

    style:

--- flake.nix
+++ flake.nix
@@ -8,78 +8,89 @@
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
-    imports = [
-      inputs.treefmt-nix.flakeModule
-    ];
-    systems = import inputs.systems;
-    perSystem = { config, lib, pkgs, system, ... }:
-      let
-        inputFile = "./src/main.typ";
-        outputFile = "./src/main.pdf";
-        fonts = with pkgs; [
-          noto-fonts-cjk-sans
-          noto-fonts-cjk-serif
-          ipafont
-        ];
-        font-paths = builtins.concatStringsSep ":" fonts;
-        typst-compile = pkgs.writeShellScriptBin "compile" ''
-          export TYPST_FONT_PATHS=${font-paths}
-          ${pkgs.typst}/bin/typst compile \
-            --ignore-system-fonts \
-            ${inputFile} \
-            ${outputFile}
-        '';
-        typst-fonts = pkgs.writeShellScriptBin "fonts" ''
-          export TYPST_FONT_PATHS=${font-paths}
-          ${pkgs.typst}/bin/typst fonts \
-            --ignore-system-fonts \
-        '';
-      in
-      {
-      # _module.args.pkgs = import self.inputs.nixpkgs {
-      #   inherit system;
-      #   config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-      #   ];
-      # };
-
-      treefmt = {
-        flakeCheck = true;
-        flakeFormatter = true;
-        programs = {
-          nixfmt.enable = true;
-          typstyle.enable = true;
-        };
-      };
-
-      apps = {
-        "compile" = {
-          type = "app";
-          program = typst-compile;
-        };
-        "fonts" = {
-          type = "app";
-          program = typst-fonts;
-        };
-
-      };
-
-      devShells = {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            typst
-            # corefonts
+  outputs =
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      systems = import inputs.systems;
+      perSystem =
+        {
+          config,
+          lib,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          inputFile = "./src/main.typ";
+          outputFile = "./src/main.pdf";
+          fonts = with pkgs; [
+            noto-fonts-cjk-sans
+            noto-fonts-cjk-serif
             ipafont
-          ]
-          ++ [
-            typst-compile
-            typst-fonts
           ];
-          shellHook = ''
+          font-paths = builtins.concatStringsSep ":" fonts;
+          typst-compile = pkgs.writeShellScriptBin "compile" ''
             export TYPST_FONT_PATHS=${font-paths}
+            ${pkgs.typst}/bin/typst compile \
+              --ignore-system-fonts \
+              ${inputFile} \
+              ${outputFile}
           '';
+          typst-fonts = pkgs.writeShellScriptBin "fonts" ''
+            export TYPST_FONT_PATHS=${font-paths}
+            ${pkgs.typst}/bin/typst fonts \
+              --ignore-system-fonts \
+          '';
+        in
+        {
+          # _module.args.pkgs = import self.inputs.nixpkgs {
+          #   inherit system;
+          #   config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+          #   ];
+          # };
+
+          treefmt = {
+            flakeCheck = true;
+            flakeFormatter = true;
+            programs = {
+              nixfmt.enable = true;
+              typstyle.enable = true;
+            };
+          };
+
+          apps = {
+            "compile" = {
+              type = "app";
+              program = typst-compile;
+            };
+            "fonts" = {
+              type = "app";
+              program = typst-fonts;
+            };
+
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              packages =
+                with pkgs;
+                [
+                  typst
+                  # corefonts
+                  ipafont
+                ]
+                ++ [
+                  typst-compile
+                  typst-fonts
+                ];
+              shellHook = ''
+                export TYPST_FONT_PATHS=${font-paths}
+              '';
+            };
+          };
         };
-      };
     };
-  };
 }
